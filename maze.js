873ceb0dc02dbb243ecc7bbb890b4990b4614c9c
DreLaing <drelaing876@gmail.com>
2018-10-23 22:38:11 -0500

    exercise 1

---
+++ maze.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+
+//exercise1
+	let boundary1 = document.getElementById("boundary1");
+	
+	boundary1.onmouseover = () =>{
+		boundary1.setAttribute("class", "boundary youlose");
+
+	};
+
+
+
+};
